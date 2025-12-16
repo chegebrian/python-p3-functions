f7103c9aacad9ae827902792e0358b9f155a87f1
brianwaithaka <waithakabrian8@gmail.com>
2025-12-16 05:36:56 +0300

    complete lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,41 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+
+greet_programmer()
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+
+greet("Naureen")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+
+greet_with_default()
+greet_with_default("Sunny")
+
 
 def add(num1, num2):
-    pass
+    return (num1 + num2)
+
+
+add(1, 2)
+
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    else:
+        return number / 2
+
+
+result = halve(4)
+result = halve("two")
+print(result)
